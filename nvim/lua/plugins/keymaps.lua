e935bdabdae1b24b42a76e0063641bbe986641f0
Alexander Galea <agalea91@gmail.com>
2024-05-20 15:32:51 -0400

    Remove spider

--- nvim/lua/plugins/keymaps.lua
+++ nvim/lua/plugins/keymaps.lua
@@ -27,11 +27,6 @@ vim.keymap.set("n", "<leader>mp", ":MarkdownPreviewToggle<cr>")
 -- nvim-comment
 vim.keymap.set({"n", "v"}, "<leader>/", ":CommentToggle<cr>")
 
--- spider
--- vim.keymap.set({ "n", "o", "x" }, "<leader>w", "<cmd>lua require('spider').motion('w')<CR>", { desc = "Spider-w" })
--- vim.keymap.set({ "n", "o", "x" }, "<leader>e", "<cmd>lua require('spider').motion('e')<CR>", { desc = "Spider-e" })
--- vim.keymap.set({ "n", "o", "x" }, "<leader>b", "<cmd>lua require('spider').motion('b')<CR>", { desc = "Spider-b" })
-
 ------------------
 -- goto-preview --
 ------------------
