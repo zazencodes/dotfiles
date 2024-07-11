ff674f3b50b97ec7a67005fa5742aa4375c43c43
Alexander Galea <agalea91@gmail.com>
2024-07-11 11:00:31 -0400

    Add spellchecking

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -1,5 +1,6 @@
 -- vim.g.python3_host_prog = os.getenv("HOME") .. '/virtualenvs/nvim-venv/bin/python' -- python code environment
 
+vim.opt.spell = true
 vim.opt.encoding = "utf-8" -- set encoding
 vim.opt.nu = true -- enable line numbers
 vim.opt.relativenumber = true -- relative line numbers
