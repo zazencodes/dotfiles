a1b9e75210d40c9fc2f3e914b297e3a1cf227dc6
Alexander Galea <agalea91@gmail.com>
2024-09-10 19:09:29 -0400

    Add dadbod
    Update 2024-09-12

--- nvim/lua/plugins/lazy.lua
+++ nvim/lua/plugins/lazy.lua
@@ -248,5 +248,25 @@ require('lazy').setup({
   -- Treesitter playground
   { "nvim-treesitter/nvim-treesitter" },
   { "nvim-treesitter/playground" },
+
+  {
+    'kristijanhusak/vim-dadbod-ui',
+    dependencies = {
+      { 'tpope/vim-dadbod', lazy = true },
+      { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true }, -- Optional
+    },
+    cmd = {
+      'DBUI',
+      'DBUIToggle',
+      'DBUIAddConnection',
+      'DBUIFindBuffer',
+    },
+    init = function()
+      -- Your DBUI configuration
+      vim.g.db_ui_use_nerd_fonts = 1
+      vim.g.db_ui_execute_on_save = 0
+    end,
+  },
+
 })
 
