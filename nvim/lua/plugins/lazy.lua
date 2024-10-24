7c9a371cfbf97f472ce56763bbd830894613f448
Alexander Galea <agalea91@gmail.com>
2024-10-24 09:22:58 -0400

    Add avante

--- nvim/lua/plugins/lazy.lua
+++ nvim/lua/plugins/lazy.lua
@@ -149,7 +149,7 @@ require('lazy').setup({
     end
   },
 
-  {'akinsho/bufferline.nvim', version = "*", dependencies = 'nvim-tree/nvim-web-devicons'},
+  {'akinsho/bufferline.nvim', dependencies = 'nvim-tree/nvim-web-devicons'},
 
   -- { "chrisgrieser/nvim-spider" },
 
@@ -267,6 +267,61 @@ require('lazy').setup({
       vim.g.db_ui_execute_on_save = 0
     end,
   },
+
+  {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    lazy = false,
+    version = false, -- set this if you want to always pull the latest change
+    opts = {
+      provider = "openai",
+      openai = {
+        -- endpoint = "https://api.openai.com/v1",
+        model = "gpt-4o-mini",
+        -- timeout = 30000, -- Timeout in milliseconds
+        -- temperature = 0,
+        -- max_tokens = 4096,
+        -- ["local"] = false,
+      },
+    },
+    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+    build = "make",
+    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      --- The below dependencies are optional,
+      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+      "zbirenbaum/copilot.lua", -- for providers='copilot'
+      {
+        -- support for image pasting
+        "HakonHarnes/img-clip.nvim",
+        event = "VeryLazy",
+        opts = {
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            -- required for Windows users
+            use_absolute_path = true,
+          },
+        },
+      },
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        'MeanderingProgrammer/render-markdown.nvim',
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+      },
+    },
+  }
 
 })
 
