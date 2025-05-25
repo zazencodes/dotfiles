44f041806a5149ca0fccbebfd0fc72fae38834dd
Alexander Galea <agalea91@gmail.com>
2025-05-24 22:06:23 -0400

    Add code companion keymaps

--- nvim/lua/plugins/options.lua
+++ nvim/lua/plugins/options.lua
@@ -7,3 +7,11 @@ require("bufferline").setup{} --bufferline
 
 vim.o.conceallevel = 2 -- set conceal level for obsidian plugin
 
+
+-- codecompanion.nvim
+vim.keymap.set({ "n", "v" }, "<leader>oa", "<cmd>CodeCompanionActions<cr>", { noremap = true, silent = true })
+vim.keymap.set({ "n", "v" }, "<leader>oe", "<cmd>CodeCompanionChat Toggle<cr>", { noremap = true, silent = true })
+vim.keymap.set("v", "ga", "<cmd>CodeCompanionChat Add<cr>", { noremap = true, silent = true })
+vim.cmd([[cab cc CodeCompanion]]) -- Expand 'cc' into 'CodeCompanion' in the command line
+
+
