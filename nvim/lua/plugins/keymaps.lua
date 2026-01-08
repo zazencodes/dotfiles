335d404b3e877d1c0b1678e605b25f6234cd382f
Alexander Galea <agalea91@gmail.com>
2026-01-08 10:21:41 +0900

    Update telescope commands

--- nvim/lua/plugins/keymaps.lua
+++ nvim/lua/plugins/keymaps.lua
@@ -1,9 +1,10 @@
 -- telescope
-vim.keymap.set("n", "<leader>fs", ":Telescope find_files<cr>")
-vim.keymap.set("n", "<leader>fp", ":Telescope git_files<cr>")
-vim.keymap.set("n", "<leader>fz", ":Telescope live_grep<cr>")
-vim.keymap.set("n", "<leader>fo", ":Telescope oldfiles<cr>")
-vim.keymap.set("n", "<leader>fb", ":Telescope buffers<cr>")
+vim.keymap.set("n", "<leader>fs", ":Telescope find_files<cr>") -- search project
+vim.keymap.set("n", "<leader>fp", ":Telescope git_files<cr>")  -- search git
+vim.keymap.set("n", "<leader>fo", ":Telescope oldfiles<cr>")   -- search old files
+vim.keymap.set("n", "<leader>fb", ":Telescope buffers<cr>")    -- search buffers
+vim.keymap.set("n", "<leader>fz", ":Telescope live_grep<cr>")                       -- grep whole project
+vim.keymap.set("n", "<leader>fg", ":Telescope live_grep grep_open_files=true<cr>")  -- grep open files
 vim.keymap.set("n", "<leader>fh", ":Telescope help_tags<cr>")
 
 -- tree
